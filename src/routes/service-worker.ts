fccafecadb65592eeb1ab9ebe66eec014fe76a33
Talha-Bayansar <talha.bayansar@hotmail.com>
2023-11-17 14:39:27 +0100

    another attempt to implement offline mode

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -8,6 +8,35 @@
  * You can also use this file to add more functionality that runs in the service worker.
  */
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+import { CacheFirst } from "workbox-strategies";
+
+const revision = import.meta.env.VITE_GIT_COMMIT_HASH;
+
+precacheAndRoute([
+  { url: "/", revision },
+  { url: "/no-access", revision },
+  { url: "/settings", revision },
+  { url: "/signin", revision },
+  { url: "/manifest.json", revision },
+  { url: "/icon512_maskable.png", revision },
+  { url: "/icon512_rounded.png", revision },
+]);
+cleanupOutdatedCaches();
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/")));
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/no-access")));
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/settings")));
+registerRoute(new NavigationRoute(createHandlerBoundToURL("/signin")));
+registerRoute(
+  ({ request }) =>
+    request.destination === "style" || request.destination === "image",
+  new CacheFirst(),
+);
 
 setupServiceWorker();
 
