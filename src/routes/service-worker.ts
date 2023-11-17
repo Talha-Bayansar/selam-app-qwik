3f7e42daefa27e24ed85e71e9eb4c68ea0fc7390
Talha-Bayansar <talha.bayansar@hotmail.com>
2023-11-17 12:24:32 +0100

    testing new service worker

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -7,12 +7,29 @@
  * Qwik uses a service worker to speed up your site and reduce latency, ie, not used in the traditional way of offline.
  * You can also use this file to add more functionality that runs in the service worker.
  */
-import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
+// import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
 
-setupServiceWorker();
+// setupServiceWorker();
 
-addEventListener("install", () => self.skipWaiting());
+// addEventListener("install", (e) => {
+//   e.waitUntil();
+// });
 
-addEventListener("activate", () => self.clients.claim());
+// addEventListener("activate", () => self.clients.claim());
 
-declare const self: ServiceWorkerGlobalScope;
+// declare const self: ServiceWorkerGlobalScope;
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install", e);
+  // e.waitUntil(
+  //   (async () => {
+  //     const cache = await caches.open(cacheName);
+  //     console.log("[Service Worker] Caching all: app shell and content");
+  //     await cache.addAll(contentToCache);
+  //   })(),
+  // );
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] Activate", e);
+});
