7520db782d572d749291e7b0c5ea2fa427b9c1ff
Talha-Bayansar <talha.bayansar@hotmail.com>
2023-11-17 18:48:49 +0100

    offline fallback

--- src/routes/service-worker.ts
+++ src/routes/service-worker.ts
@@ -8,36 +8,14 @@
  * You can also use this file to add more functionality that runs in the service worker.
  */
 import { setupServiceWorker } from "@builder.io/qwik-city/service-worker";
-import { precacheAndRoute } from "workbox-precaching";
-import {
-  NavigationRoute,
-  registerRoute,
-  setDefaultHandler,
-} from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
 
-const revision = Date.now().toString();
+import { offlineFallback } from "workbox-recipes";
+import { setDefaultHandler } from "workbox-routing";
+import { NetworkOnly } from "workbox-strategies";
 
-precacheAndRoute([{ url: "/offline", revision }]);
+setDefaultHandler(new NetworkOnly());
 
-// Set up a route to handle navigation requests
-registerRoute(
-  new NavigationRoute(async ({ event }) => {
-    // Check if the browser is offline
-    if (!navigator.onLine) {
-      const response = await caches.match("/offline");
-      if (response) {
-        return response;
-      } // Redirect navigation to offline page
-    }
-    const e = event as FetchEvent;
-    // Otherwise, proceed with the requested URL
-    return fetch(e.request);
-  }),
-);
-
-// Set the default handler for non-navigation requests
-setDefaultHandler(new StaleWhileRevalidate());
+offlineFallback();
 
 setupServiceWorker();
 
